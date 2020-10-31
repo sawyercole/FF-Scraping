a3f2624302749630843936e57e74ce3f6d0d68de
Sawyer Cole <sawyercole50@gmail.com>
2020-10-31 00:55:34 -0700

    Update csvWriting.py

--- csvWriting.py
+++ csvWriting.py
@@ -3,13 +3,13 @@ import os
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import re
-#player = input("Player: ")
-#compare_weeks = [input("Compare Week: "), input('And Week:')]
+leagueID = input("Enter League ID: ")
 
 
 
-def getTeamId(player, season) :
-	url = 'https://fantasy.nfl.com/league/4007290/history/' + season + '/owners'
+#gets the team id number from a player name
+def getteamid(player, season) :
+	url = 'https://fantasy.nfl.com/league/' + leagueID + '/history/' + season + '/owners'
 	page = urlopen(url)
 	html = page.read()
 	page.close()
@@ -19,102 +19,104 @@ def getTeamId(player, season) :
 		if teamWrap.find('td', class_ = 'teamOwnerName').text.strip() == player :
 			return teamWrap.attrs['class'][0].split('-')[1]
 
-def compareRoster(teamId, season, start, end) :
-	start_roster = getRoster(teamId, season, start)
-	end_roster = getRoster(teamId, season, end)
+#gets the total numver of players in a given season
+def get_numberofowners(season) :
+	owners_url = 'https://fantasy.nfl.com/league/' + leagueID + '/history/' + season + '/owners'
+	owners_page = urlopen(owners_url)
+	owners_html = owners_page.read()
+	owners_page.close()
+	owners_soup = bs(owners_html, 'html.parser')
+	number_of_owners = len(owners_soup.find_all('tr', class_ = re.compile('team-')))
+	return number_of_owners
 
-	similar_count = 0
-	for player in end_roster :
-		if player in start_roster :
-			similar_count += 1
+#gets one row of the csv file
+#each row is the weekly data for one team in the league
+def getrow(teamId, season, week) : 
 
-	return similar_count
-
-def getRoster(teamId, season, week) :
-	page = urlopen('https://fantasy.nfl.com/league/4007290/history/' + season + '/teamgamecenter?teamId=' + teamId + '&week=' + week)
+	#loads gamecenter page as soup
+	page = urlopen('https://fantasy.nfl.com/league/' + leagueID + '/history/' + season + '/teamgamecenter?teamId=' + teamId + '&week=' + week)
 	soup = bs(page.read(), 'html.parser')
 	page.close()
-	owner = soup.find('a', class_ = re.compile('userName userId')).text
+
+	owner = soup.find('a', class_ = re.compile('userName userId')).text #username of the team owner
+
 	starters = soup.find('div', id = 'tableWrap-1').find_all('td', class_ = 'playerNameAndInfo')
 	starters = [starter.text for starter in starters]
 	bench = soup.find('div', id = 'tableWrapBN-1').find_all('td', class_ = 'playerNameAndInfo')
 	bench = [benchplayer.text for benchplayer in bench]
+	roster = starters + bench #every player on the team roster, in the order they are listed in game center, for the given week
 
 	player_totals = soup.find('div', id = 'teamMatchupBoxScore').find('div', class_ = 'teamWrap teamWrap-1').find_all('td', class_ = re.compile("statTotal"))
-	player_totals = [player.text for player in player_totals]
+	player_totals = [player.text for player in player_totals] #point totals for each player with indecies which correspond to that player's index in roster
 
-	roster = starters + bench
-	rosterandtotals = []
-	teamtotals = soup.findAll('div', class_ = re.compile('teamTotal teamId-'))
+	teamtotals = soup.findAll('div', class_ = re.compile('teamTotal teamId-')) #the team's total points for the week
 	ranktext = soup.find('span', class_ = re.compile('teamRank teamId-')).text
-	rank = ranktext[ranktext.index('(') + 1: ranktext.index(')')]
+	rank = ranktext[ranktext.index('(') + 1: ranktext.index(')')] , #the team's rank in the standings
+
+	rosterandtotals = [] #alternating player names and their corresponding weekly point totals
 	for i in range(len(roster)) :
 	 	rosterandtotals.append(roster[i])
 	 	rosterandtotals.append(player_totals[i])
 
+	#try except statement is for the situation where the league member would not have an opponent for the week
+	#in this case the Opponent and Opponent Total columns are filled with N/A
 	try:
-		rosterandtotals = [owner, rank] + rosterandtotals + [teamtotals[0].text, soup.find('div', class_ = 'teamWrap teamWrap-2').find('a', re.compile('userName userId')).text, teamtotals[1].text]
+		completed_row = [owner, rank] + rosterandtotals + [teamtotals[0].text, soup.find('div', class_ = 'teamWrap teamWrap-2').find('a', re.compile('userName userId')).text, teamtotals[1].text]
 	except:
-		rosterandtotals = [owner, rank] + rosterandtotals + [teamtotals[0].text, 'N/A', 'N/A']
-	return rosterandtotals
+		completed_row = [owner, rank] + rosterandtotals + [teamtotals[0].text, 'N/A', 'N/A']
+
+	return completed_row
 
 
 
 
-
-
+league_name = input ("League Name: ")
 season = input("Season: ")
-path = './FlatCats-League-History/' + season
 
-if !os.path.isdir(path) :
+#checks if a directory exists with that team name. If it doesn't it asks the user if it wants to create a new directory
+#to store the league data in.
+if not os.path.isdir('./' + league_name + '-League-History') :
+	if(input('No folder named ' + league_name + '-League-History found would you like to create a new folder with that name y/n?' ) == y) :
+		os.mkdir('./' + league_name + '-League-History')
+	else :
+		exit()
+
+path = './' + league_name + '-League-History/' + season #the path of the folder where the weekly csv files are stored
+
+#if that folder doesn't already exist a new one is made
+if not os.path.isdir(path) :
 	os.mkdir(path)
 
 if input("Write season data to csv y/n?") == 'y' :
+
 	#converting url to soup
-url = "https://fantasy.nfl.com/league/4007290/history/" + season + "/teamgamecenter?teamId=1&week=1"
-page = urlopen(url)
-html = page.read()
-page.close()
-soup = bs(html, 'html.parser')
+	url = "https://fantasy.nfl.com/league/" + leagueID + "/history/" + season + "/teamgamecenter?teamId=1&week=1"
+	page = urlopen(url)
+	html = page.read()
+	page.close()
+	soup = bs(html, 'html.parser')
 
-season_length = len(soup.find_all('li', class_ = re.compile('ww ww-')))
+	season_length = len(soup.find_all('li', class_ = re.compile('ww ww-'))) #determines how may unique csv files are created, total number of weeks in the season
+	number_of_owners = get_numberofowners(season) #number of teams in the league
 
-owners_url = 'https://fantasy.nfl.com/league/4007290/history/' + season + '/owners'
-owners_page = urlopen(owners_url)
-owners_html = owners_page.read()
-owners_page.close()
-owners_soup = bs(owners_html, 'html.parser')
-number_of_owners = len(owners_soup.find_all('tr', class_ = re.compile('team-')))
-print(number_of_owners)
+	position_tags = [tag.text for tag in soup.find('div', id = 'teamMatchupBoxScore').find('div', class_ = 'teamWrap teamWrap-1').find_all('span', class_ = 'final')]
+	#position tags are the label for each starting roster spot. different leagues can have different configurations for their starting rosters
 
-position_tags = [tag.text for tag in soup.find('div', id = 'teamMatchupBoxScore').find('div', class_ = 'teamWrap teamWrap-1').find_all('span', class_ = 'final')]
-header = []
+	header = [] #csv file header
 
-for i in range(len(position_tags)) :
-	header.append(position_tags[i])
-	header.append('Points')
+	#adds the position tags to the header. each tag is followed by a column to record the player's points for the week
+	for i in range(len(position_tags)) :
+		header.append(position_tags[i])
+		header.append('Points')
 
-header = ['Owner',  'Rank'] + header + ['Total', 'Opponent', 'Opponent Total']
+	header = ['Owner',  'Rank'] + header + ['Total', 'Opponent', 'Opponent Total']
 
-for i in range(1, season_length + 1) :
-	with open('./FlatCats-League-History/' + season + '/' + str(i) + '.csv', 'w', newline='') as f :
-		writer = csv.writer(f)
-		writer.writerow(header)
-		for j in range(1, number_of_owners + 1) :
-			writer.writerow(getRoster(str(j), season, str(i)))
-		
 
-	print(i)
-
-#print('These rosters share ' + str(compareRoster(getTeamId(player, season), season, compare_weeks[0], compare_weeks[1])) + ' common players')
-
-# url = 'https://fantasy.nfl.com/league/4007290/history/' + season + '/teamgamecenter?teamId=' + getTeamId(player, season) + '&week=' + week
-# page = urlopen(url)
-# html = page.read()
-# page.close()
-# soup = bs(html, 'html.parser')
-# player_table = soup.find('div', id = 'tableWrap-1')
-# names = player_table.find_all('td', class_ = 'playerNameAndInfo')
-# for name in names :
-# 	print (name.text)
-
+	for i in range(1, season_length + 1) : #iterates through each week of the season, creating a new csv file every loop
+		with open('./FlatCats-League-History/' + season + '/' + str(i) + '.csv', 'w', newline='') as f :
+			writer = csv.writer(f)
+			writer.writerow(header) #writes header as the first line in the new csv file
+			for j in range(1, number_of_owners + 1) : #iterates through every team owner
+				writer.writerow(getrow(str(j), season, str(i))) #writes a row for each owner in the csv
+		print("Week " + str(i) + " Complete")
+	print("Done")
